7128226d48a9c2b47988290aa3d533058f98f267
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-05 19:42:01 -0500

    Hovering divs now colors them through classes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SIZE = 16;
-const grid = document.querySelector('#container');
+const SIZE = 32;
+const grid = document.querySelector('#grid');
 
 const gridSize = grid.clientWidth;
 const itemSize = gridSize / SIZE;
@@ -7,11 +7,14 @@ const itemSize = gridSize / SIZE;
 // Each div size is relative to the grid size
 for (let i = 0; i < SIZE * SIZE; i++) {
     const div = document.createElement('div');
-    div.setAttribute(
-        'style', 
-        `width: ${itemSize}px; 
-        height: ${itemSize}px;
-        border: 1px solid #3f3f3f;
-        box-sizing: border-box;`);
+    div.style.height = `${itemSize}px`;
+    div.style.width = `${itemSize}px`;
     grid.appendChild(div);
+
+    div.addEventListener('mouseover', (event) => {
+        div.classList.add('color');
+    });
 }
+
+// string = '#' + Math.floor(Math.random()*16777215).toString(16);
+// div.style.backgroundColor = string;

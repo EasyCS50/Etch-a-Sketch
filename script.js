17c93e38b8b8ce5d385229276637f45461f0e7bf
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-08 17:37:15 -0500

    Created a createGrid function

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const SIZE = 32;
+const SIZE = 24;
 const grid = document.querySelector('#grid');
 
 const gridSize = grid.clientWidth;
-const itemSize = gridSize / SIZE;
+const divSize = gridSize / SIZE;
 
-// Each div size is relative to the grid size
-for (let i = 0; i < SIZE * SIZE; i++) {
-    const div = document.createElement('div');
-    div.style.height = `${itemSize}px`;
-    div.style.width = `${itemSize}px`;
-    grid.appendChild(div);
+createGrid(SIZE);
 
-    div.addEventListener('mouseover', (event) => {
-        div.classList.add('color');
-    });
+function createGrid(squares) {
+    // Each div size is relative to the grid size
+    for (let i = 0; i < squares * squares; i++) {
+        const div = document.createElement('div');
+        div.style.height = `${divSize}px`;
+        div.style.width = `${divSize}px`;
+        grid.appendChild(div);
+
+        div.addEventListener('mouseover', (event) => {
+            div.classList.add('color');
+        });
+    }
 }
-
 // string = '#' + Math.floor(Math.random()*16777215).toString(16);
 // div.style.backgroundColor = string;

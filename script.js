bb1dabc65546b6959261214046ef45db778fb5a0
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-11 16:51:59 -0500

    Added clear, black and random color event listeners

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ const slider = document.querySelector("#slider");
 const sliderText = document.querySelector("#slider-text");
 const change = document.querySelector("#change");
 const clear = document.querySelector("#clear");
+const black = document.querySelector("#black");
+const random = document.querySelector("#random")
 const grid = document.querySelector("#grid");
 
 const gridSize = grid.clientWidth;
 const DEFAULT_SIZE = 16;
+let randomClicked = false;
+
+slider.addEventListener("mousemove", () => {
+    let num = slider.value;
+    sliderText.innerHTML = `${num} x ${num}`;
+})
+
+black.addEventListener("click", () => {
+    randomClicked = false;
+})
+
+random.addEventListener("click", () => {
+    randomClicked = true;
+})
 
 function initializeGrid(size) {
   const divSize = gridSize / size;
   createGrid(size, divSize);
-}
-
-function updateSlider() {
-  let num = slider.value;
-  sliderText.innerHTML = `${num} x ${num}`;
 }
 
 function updateGrid() {
@@ -33,6 +44,10 @@ function createGrid(squares, divSize) {
     grid.appendChild(div);
 
     div.addEventListener("mouseover", (event) => {
+      if (randomClicked === true) {
+        string = '#' + Math.floor(Math.random()*16777215).toString(16);
+        div.style.backgroundColor = string;
+      }
       div.classList.add("color");
     });
   }
@@ -47,7 +62,6 @@ function deleteGrid() {
 initializeGrid(DEFAULT_SIZE);
 sliderText.innerHTML = `${DEFAULT_SIZE} x ${DEFAULT_SIZE}`;
 
-slider.addEventListener("mousemove", updateSlider);
 change.addEventListener("click", updateGrid);
 clear.addEventListener("click", updateGrid);
 // string = '#' + Math.floor(Math.random()*16777215).toString(16);
